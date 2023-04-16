286b5e04e3291d681ac75d4dbff49073c82acb3b
Isquare007 <akibrahiim2019@gmail.com>
2023-04-16 01:13:15 +0100

    hyper resilient Tak 3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """persists data even after deletion"""
+        dataset = self.indexed_dataset()
+        data_length = len(dataset)
+
+        assert index is None or (0 <= index < data_length)
+        start_index = 0 if index is None else index
+        data = [dataset[i]
+                for i in range(start_index, start_index + page_size)]
+        end_index = start_index + len(data)
+        next_index = end_index
+
+        while next_index < data_length and next_index in dataset:
+            next_index += 1
+        return {
+            "index": start_index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
+
+
+if __name__ == "__main__":
+    server = Server()
+
+    server.indexed_dataset()
+
+    try:
+        server.get_hyper_index(300000, 100)
+    except AssertionError:
+        print("AssertionError raised when out of range")
+
+    index = 3
+    page_size = 2
+
+    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+    # 1- request first index
+    res = server.get_hyper_index(index, page_size)
+    print(res)
