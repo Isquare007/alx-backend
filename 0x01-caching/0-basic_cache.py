6f5e5827a21063df99e9ce043323387c499f45d0
Isquare007 <akibrahiim2019@gmail.com>
2023-04-18 14:19:24 +0100

    task 0 Basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""BasicCache Module"""
+BaseCaching = __import__('basic_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """class BasicCache that inherits from
+    BaseCaching and is a caching system"""
+
+    def put(self, key, item):
+        if item and key is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data[key]
