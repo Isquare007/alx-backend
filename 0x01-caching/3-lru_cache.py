6f33bdd7d3ce7ac92a1ccd7c64cdb37c9cdb66d3
Isquare007 <akibrahiim2019@gmail.com>
2023-04-19 17:31:13 +0100

    3- LRU

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,59 +1,43 @@
 #!/usr/bin/env python3
-"""LRU Cache Module"""
-BaseCaching = __import__('basic_caching').BaseCaching
+"""
+LRU caching
+"""
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    def __init__(self) -> None:
-        """initialize"""
+    """"
+    inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initializing LRUCache
+        """
         super().__init__()
-        self.keyys = []
+        self.queue = []
 
     def put(self, key, item):
-        """ Add an item in the cache with LRU algorithm
+        """Assign key and item
         """
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS\
-                and key not in self.keyys:
-            discard = self.keyys.pop(0)
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.queue:
+            discard = self.queue.pop(0)  # Dequeue key
             del self.cache_data[discard]
             print("DISCARD: {}".format(discard))
+
         if key and item:
             if key in self.cache_data:
-                self.keyys.remove(key)
-            self.keyys.append(key)
+                self.queue.remove(key)  # remove from the queue
+            self.queue.append(key)  # enqueue key
             self.cache_data[key] = item
 
     def get(self, key):
-        if key not in self.cache_data or key is None:
+        """Fetch data with key
+        """
+        if not key or key not in self.cache_data:
             return None
-        self.keyys.remove(key)
-        self.keyys.append(key)
+        # Remove from any position in the queue and add to the back
+        self.queue.remove(key)
+        self.queue.append(key)
+
         return self.cache_data[key]
-
-
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
