a34288a2575a8d223213f7708a5b14175618cff4
Isquare007 <akibrahiim2019@gmail.com>
2023-04-19 17:27:49 +0100

    2 lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,43 +1,37 @@
 #!/usr/bin/env python3
-"""LIFO Cache Module"""
-BaseCaching = __import__('basic_caching').BaseCaching
+"""
+LIFO caching
+"""
+from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    def __init__(self) -> None:
-        """initialize"""
+    """
+    Inherits from BaseCaching
+    """
+    def __init__(self):
+        """
+        Initializing LIFO caching
+        """
         super().__init__()
-        self.keyys = []
+        self.stack = []
 
     def put(self, key, item):
-        """ Add an item in the cache with LIFO algorithm
         """
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS\
-                and key not in self.keyys:
-            discard = self.keyys.pop()
+        Assigning key and item
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.stack:
+            discard = self.stack.pop()
             del self.cache_data[discard]
             print("DISCARD: {}".format(discard))
         if key and item:
-            self.keyys.append(key)
+            self.stack.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        if key not in self.cache_data or key is None:
+        """
+        Fetch data with key
+        """
+        if not key or key not in self.cache_data.keys():
             return None
-        return self.cache_data[key]
-
-
-my_cache = LIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
+        return self.cache_data.get(key)
