4a6f47d769a7dece1b1337185aab895d91e41061
Isquare007 <akibrahiim2019@gmail.com>
2023-04-19 16:45:00 +0100

    1 amend

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,26 +13,12 @@ class FIFOCache(BaseCaching):
         """
         if item and key is not None:
             self.cache_data[key] = item
+
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 discard = next(iter(self.cache_data.keys()))
                 self.cache_data.pop(discard)
                 print("DISCARD: {}".format(discard[0]))
-        return
 
     def get(self, key):
         """get value of key passed"""
         return self.cache_data.get(key)
-
-
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
