294291a130e7aeefee93951a05b71cdbfdecf427
Isquare007 <akibrahiim2019@gmail.com>
2023-04-25 16:54:58 +0100

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,44 +1,40 @@
 #!/usr/bin/env python3
-"""
-a simple flask app
-"""
-from flask import Flask, render_template
+"""Basic Babel Setup"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
+class Config:
+    """Config class for flask app
+    """
+    DEBUG = True
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
-class Config(object):
-    """
-    class used  to configure languages
-    """
-    LANGUAGES = ['en', 'fr']
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-
 @app.route('/')
-def hello_world():
+def index():
+    """view function for root route
+    Returns:
+        html: homepage
     """
-    outputs hello world
-    """
-    return render_template("2-index.html")
+    return render_template('2-index.html')
 
 
 @babel.localeselector
 def get_locale():
+    """get best language match
+    Returns:
+        str: best match
     """
-    get locale
-    """
-    locale = request.accept_languages.best_match(app.config['LANGUAGES'])
-    return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
